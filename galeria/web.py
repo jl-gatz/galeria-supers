5f29477a2e0d573658a561703d1df8b70dd2f144
J-L Gatz <elan.rgb@gmail.com>
2026-06-01 09:08:09 -0300

    chore: Adaptação no web.py para o Render

--- galeria/web.py
+++ galeria/web.py
@@ -4,6 +4,9 @@ import flet as ft
 
 from galeria.main import main
 
+os.environ.setdefault("FLET_FORCE_WEB_SERVER", "true")
+os.environ.setdefault("FLET_SERVER_IP", "0.0.0.0")
+
 if "PORT" in os.environ:
     os.environ["FLET_SERVER_PORT"] = os.environ["PORT"]
 
